998a510edb389b6b4415fb428a6d71d84475746b
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-12-14 19:53:21 -0300

    create button component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/button'
 import { Steps } from '@/components/steps'
 import { Welcome } from '@/components/welcome'
 import { View } from 'react-native'
@@ -6,5 +7,8 @@ export default function Index() {
   return <View style={{ flex: 1, padding: 40, gap: 40 }}>
     <Welcome />
     <Steps />
+    <Button>
+      <Button.Title>Clique aqui</Button.Title>
+    </Button>
   </View>
 }
